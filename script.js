461d87e406e5be3371510104f678b76881d367bd
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-11 19:49:18 +0200

    Added UI. Started script. Added player.

---
+++ script.js
@@ -0,0 +1,11 @@
+const timeLeftDisplay = document.querySelector("#time-left");
+const resultDisplay = document.querySelector("#result");
+const startPauseButton = document.querySelector("#start-pause-btn");
+
+const squares = document.querySelectorAll(".grid div");
+
+function moveFrog() {
+  console.log("moved");
+}
+
+document.addEventListener("keyup", moveFrog);
