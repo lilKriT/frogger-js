c20edd8416ce7ebd200f0c1d70530356a43729f8
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-11 20:22:24 +0200

    Moving player.

--- script.js
+++ script.js
@@ -3,9 +3,32 @@ const resultDisplay = document.querySelector("#result");
 const startPauseButton = document.querySelector("#start-pause-btn");
 
 const squares = document.querySelectorAll(".grid div");
+let currentIndex = 76;
 
-function moveFrog() {
-  console.log("moved");
+const width = 9;
+
+function moveFrog(e) {
+  squares[currentIndex].classList.remove("frog");
+
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentIndex % width != 0) currentIndex--;
+      break;
+    case "ArrowRight":
+      if (currentIndex % width < width - 1) currentIndex++;
+      break;
+    case "ArrowUp":
+      if (currentIndex >= width) currentIndex -= width;
+      break;
+    case "ArrowDown":
+      if (currentIndex + width < width * width) currentIndex += width;
+      break;
+
+    default:
+      break;
+  }
+
+  squares[currentIndex].classList.add("frog");
 }
 
 document.addEventListener("keyup", moveFrog);
